009ca219ae9693d448590dc92598a46fca72abe7
acuyjet <allison@cuyjet.com>
2022-10-04 08:42:53 -0400

    Get basic login test working

---
+++ PageObjects/LoginPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace HudlTests.PageObjects
+{
+    public class LoginPage
+    {
+        public IWebDriver driver;
+
+        public void Setup()
+        {
+            
+            var outputDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            driver = new ChromeDriver(outputDirectory);
+            driver.Navigate().GoToUrl("https://www.hudl.com/login");
+
+        }
+
+        public void Login(string email, string password)
+        {
+            EmailField.Clear();
+            PasswordField.Clear();
+            EmailField.SendKeys(email);
+            PasswordField.SendKeys(password);
+            LoginButton.Click();
+        }
+
+        private IWebElement EmailField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("email"));
+            }
+        }
+
+        private IWebElement PasswordField
+        {
+            get
+            {
+                return driver.FindElement(By.Id("password"));
+            }
+        }
+
+        private IWebElement LoginButton
+        {
+            get
+            {
+                return driver.FindElement(By.CssSelector("button[data-qa-id='login-btn']"));
+            }
+        }
+    }
+
+}
+
---
+++ WebTests/LoginTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using HudlTests.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace HudlTests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        //IWebDriver driver;
+
+        LoginPage loginPage = new LoginPage();
+
+        [TestMethod]
+        public void LoginSuccess()
+        {
+            var email = "";
+            var password = "";
+            loginPage.Setup();
+
+            loginPage.Login(email, password);
+
+            Assert.IsTrue(loginPage.driver.Url.Contains("hudl.com/home"), "Login unsuccessful.");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            loginPage.driver.Quit();
+        }
+    }
+}
+
